8b3916e812d22eaa9ef6f6ef5cdaaa0815cfcf3d
ahammednibras8 <ahammednibras@proton.me>
2024-12-16 15:30:22 +0530

    Redesigned app bar with dynamic user greeting and profile image

--- lib/features/authentication/views/onboarding_screen.dart
+++ lib/features/authentication/views/onboarding_screen.dart
@@ -3,9 +3,9 @@ import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:spare_boy/core/constants/app_colors.dart';
 import 'package:spare_boy/features/authentication/views/login.dart';
-import 'package:spare_boy/features/authentication/views/sign_up.dart';
 import 'package:spare_boy/features/common/widgets/buttons.dart';
 import 'package:spare_boy/features/common/widgets/circle_avatar.dart';
+import 'package:spare_boy/features/home/home_screen.dart';
 
 class OnboardingScreen extends StatefulWidget {
   const OnboardingScreen({super.key});
@@ -84,7 +84,8 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                   Navigator.push(
                     context,
                     MaterialPageRoute(
-                      builder: (context) => const SignUpScreen(),
+                      //TODO: Rename the route
+                      builder: (context) => const HomeScreen(),
                     ),
                   );
                 },
@@ -113,9 +114,11 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
                     icon: Icons.arrow_forward_rounded,
                     onTap: () {
                       Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LoginScreen()));
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const LoginScreen(),
+                        ),
+                      );
                     },
                   )
                 ],
