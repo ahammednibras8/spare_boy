f86fada084740f60d76b7821564e9574489d9ae5
yahy-a <133936738+yahy-a@users.noreply.github.com>
2024-12-16 12:45:23 +0530

    commit

--- lib/features/authentication/views/sign_up.dart
+++ lib/features/authentication/views/sign_up.dart
@@ -21,7 +21,7 @@ class _SignUpScreenState extends State<SignUpScreen> {
       body: SingleChildScrollView(
         physics: const BouncingScrollPhysics(),
         child: SizedBox(
-          height: MediaQuery.of(context).size.height ,
+          height: MediaQuery.of(context).size.height,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.end,
             children: [
@@ -55,79 +55,79 @@ class _SignUpScreenState extends State<SignUpScreen> {
               ),
               const SizedBox(height: 30),
               Form(
-                key: _formKey,
+                  key: _formKey,
                   child: Column(children: [
-                OnboardingTextField(
-                  label: 'Username',
+                    OnboardingTextField(
+                      label: 'Username',
                       validator: (value) {
-                        if(value == null || value.isEmpty) {
+                        if (value == null || value.isEmpty) {
                           return 'Please enter your username';
                         }
                         return null;
                       },
-                ),
-                OnboardingTextField(
-                  label: 'Email',
+                    ),
+                    OnboardingTextField(
+                      label: 'Email',
                       validator: (value) {
-                        if(value == null || value.isEmpty) {
+                        if (value == null || value.isEmpty) {
                           return 'Please enter your email';
                         }
                         return null;
                       },
-                ),
-                OnboardingTextField(
-                    label: 'Password',
-                      validator: (value) {
-                        if(value == null || value == '') {
-                          return 'Please enter your password';
-                        }
-                        return null;
-                      },
-                    showPassword: showPassword,
-                    suffixIcon: IconButton(
-                      icon: Icon(showPassword
-                          ? Icons.visibility
-                          : Icons.visibility_off),
-                      color: AppColors.textPrimary,
-                      onPressed: () {
-                        setState(() {
-                          showPassword = !showPassword;
-                        });
-                      },
-                    )),
-                const SizedBox(height: 15),
-                GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const LoginScreen()));
-                  },
-                  child: Text(
-                    'Already have an account?',
-                    style: GoogleFonts.nunitoSans(
-                      color: AppColors.textPrimary,
-                      fontSize: 16,
-                      fontWeight: FontWeight.w300,
-                      decoration: TextDecoration.underline,
-                      decorationThickness: 1,
                     ),
-                  ),
-                ),
-                const SizedBox(height: 5),
-                OnboardingButton(
-                  text: 'Sign Up',
-                  onPressed: () {
-                    if(_formKey.currentState!.validate()) {
-                  }},
-                  minimumSize: const Size(325, 60),
-                ),
-                const SizedBox(height: 15),
-                CancelButton(onTap: () {
-                  Navigator.pop(context);
-                }),
-                const SizedBox(height: 10),
-              ]))
+                    OnboardingTextField(
+                        label: 'Password',
+                        validator: (value) {
+                          if (value == null || value == '') {
+                            return 'Please enter your password';
+                          }
+                          return null;
+                        },
+                        showPassword: showPassword,
+                        suffixIcon: IconButton(
+                          icon: Icon(showPassword
+                              ? Icons.visibility
+                              : Icons.visibility_off),
+                          color: AppColors.textPrimary,
+                          onPressed: () {
+                            setState(() {
+                              showPassword = !showPassword;
+                            });
+                          },
+                        )),
+                    const SizedBox(height: 15),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const LoginScreen()));
+                      },
+                      child: Text(
+                        'Already have an account?',
+                        style: GoogleFonts.nunitoSans(
+                          color: AppColors.textPrimary,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w300,
+                          decoration: TextDecoration.underline,
+                          decorationThickness: 1,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    OnboardingButton(
+                      text: 'Sign Up',
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {}
+                      },
+                      minimumSize: const Size(325, 60),
+                    ),
+                    const SizedBox(height: 15),
+                    CancelButton(onTap: () {
+                      Navigator.pop(context);
+                    }),
+                    const SizedBox(height: 10),
+                  ]))
             ],
           ),
         ),
